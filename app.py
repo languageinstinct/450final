ec3ab9fe1cb73b00654d9cf7b9fd7e1636446d4b
languageinstinct <112591373+languageinstinct@users.noreply.github.com>
2025-12-08 21:06:13 -0500

    Change data file path to fema_sample.csv

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 st.title("FEMA TSA Eligibility Dashboard")
 
 @st.cache_data
-def load_data(path="IndividualAssistanceHousingRegistrantsLargeDisasters.csv"):
+def load_data(path="fema_sample.csv"):
     df = pd.read_csv(
         path,
         low_memory=False,
